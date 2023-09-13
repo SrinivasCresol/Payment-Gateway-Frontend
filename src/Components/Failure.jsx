9dd55ac1e64b631135baf9859910687c28df76b8
Srinivas <srinivas.y@cresolinfoserv.com>
2023-09-13 16:28:57 +0530

    Updated Changes

--- src/Components/Failure.jsx
+++ src/Components/Failure.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Failure() {
   return (
     <div>
       <h1>Failure, Network Error!</h1>
+      <Link to="/user">Back To Home</Link>
     </div>
-  )
+  );
 }
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -20,18 +20,22 @@ export default function Cart({ products, removeFromCart }) {
       const stripePromise = await loadStripe(
         "pk_test_51Np1quSHnUWkZzn7d8tc3ivT8T13kWLEEizX3679LrnocnMHyr7ENWQLFYB2ETw3ejqqys0kJo9QHintcCsmYeov00UbbwmWMQ"
       );
-      const res = await fetch("http://localhost:5555/create-checkout-session", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(products),
-      });
-      if (res.statusCode === 500) return;
+      const res = await fetch(
+        "https://payment-gateway-backend-hoj3.onrender.com/create-checkout-session",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(products),
+        }
+      );
+      if (res.status === 500) return;
 
       const data = await res.json();
       console.log(data);
-      stripePromise.redirectToCheckout({ sessionId: data });
+      const sessionId = data.sessionId;
+      stripePromise.redirectToCheckout({ sessionId });
     } else {
       setTimeout(() => {
         navigate("/");
@@ -42,22 +46,18 @@ export default function Cart({ products, removeFromCart }) {
   return (
     <div className="cart">
       <h2>Shopping Cart</h2>
-      {products.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <>
-          {products.map((item) => (
-            <div key={item._id} className="cart-item">
-              <h3>{item.model}</h3>
-              <p>Quantity: {item.quantity}</p>
-              <p>Price: {item.price * item.quantity}/-</p>
-              <button onClick={handlePayment}>Place Order</button>
-              <button onClick={() => removeFromCart(item)}>Remove</button>
-            </div>
-          ))}
-          <p>Total: {calculateTotal()}/-</p>
-        </>
-      )}
+      <>
+        {products.map((item) => (
+          <div key={item._id} className="cart-item">
+            <h3>{item.model}</h3>
+            <p>Quantity: {item.quantity}</p>
+            <p>Price: {item.price * item.quantity}/-</p>
+            <button onClick={() => removeFromCart(item)}>Remove</button>
+          </div>
+        ))}
+        <p>Total: {calculateTotal()}/-</p>
+        <button onClick={handlePayment}>Place Order</button>
+      </>
     </div>
   );
 }
